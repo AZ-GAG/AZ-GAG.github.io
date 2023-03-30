078056a1f26523437f008029fa221d29103f61e3
Two-Jay <djeeee1272@gmail.com>
2023-03-31 03:22:28 +0900

    POST 구현 완

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var fs_1 = __importDefault(require("fs"));
+var morgan_1 = __importDefault(require("morgan"));
 var app = (0, express_1["default"])();
-var limitTime = undefined;
 var time_file_path = 'time.txt';
-fs_1["default"].access('time.txt', fs_1["default"].constants.F_OK | fs_1["default"].constants.R_OK, function (err) {
-    if (err) {
-        console.error(err);
-        limitTime = init_limit_time(time_file_path);
-    }
-    else {
-        console.log('Time File exists');
-        limitTime = read_time_file(time_file_path);
-    }
-});
+var time_to_hell = 7;
+app.use((0, morgan_1["default"])('combined'));
+if (check_time_file() === false) {
+    write_refresh_limit_time(time_file_path);
+}
+var limitTime = read_time_file(time_file_path);
 app.get('/time', function (req, res) {
     // return the remaining time
     // remaining time = limitTime - now
     if (limitTime === undefined) {
+        res.status(500);
         res.send('No limit time');
     }
     else {
@@ -29,32 +26,63 @@ app.get('/time', function (req, res) {
         var remainTime = limitTime.getTime() - now.getTime();
         // console.log(remainTime);
         // res.send(remainTime.toString());
+        res.status(200);
         res.send(get_time_in_form_DHMS(remainTime));
     }
 });
 app.post('/time', function (req, res) {
-    // set the limit time
-    // limitTime = now + 7 days
-    limitTime = init_limit_time(time_file_path);
-    res.send('Set limit time');
+    // refresh the limit time
+    console.log('POST /time');
+    if (write_refresh_limit_time(time_file_path) === false) {
+        res.status(500);
+        res.send('Refresh failed');
+    }
+    else {
+        limitTime = read_time_file(time_file_path);
+    }
+    res.status(200);
+    res.send('Refreshed');
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
     console.log('Server Start with Limit time:', limitTime);
 });
-function init_limit_time(path) {
+function write_refresh_limit_time(path) {
     var new_limit_date = new Date();
-    new_limit_date.setDate(new_limit_date.getDate() + 7);
-    fs_1["default"].writeFileSync(path, new_limit_date.toString());
-    return new_limit_date;
+    new_limit_date.setDate(new_limit_date.getDate() + time_to_hell);
+    try {
+        fs_1["default"].writeFileSync(path, new_limit_date.toString());
+        console.log("".concat(write_refresh_limit_time.name, " : ").concat(new_limit_date));
+        return true;
+    }
+    catch (err) {
+        console.error(err);
+    }
+    return false;
+}
+function check_time_file() {
+    try {
+        fs_1["default"].accessSync('time.txt', fs_1["default"].constants.F_OK | fs_1["default"].constants.R_OK);
+        console.log('Time File exists');
+        return true;
+    }
+    catch (err) {
+        console.log('Time File does not exist');
+        return false;
+    }
 }
 function read_time_file(path) {
-    var Data = fs_1["default"].readFileSync(path).toString();
-    var date = new Date(Data);
-    if (date instanceof Date && !isNaN(date.getTime())) {
-        return date;
+    try {
+        var Data = fs_1["default"].readFileSync(path).toString();
+        var date = new Date(Data);
+        if (date instanceof Date && !isNaN(date.getTime())) {
+            return date;
+        }
     }
-    return undefined;
+    catch (err) {
+        console.error(err);
+        return undefined;
+    }
 }
 function get_time_in_form_DHMS(time_ms) {
     var diffMs = time_ms; // 밀리초 단위 차이 계산
