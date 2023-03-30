e2fbb411594c76e87a335c6119a7b4d07cea00c8
Two-Jay <djeeee1272@gmail.com>
2023-03-31 02:40:58 +0900

    정답 맞출 시 다음 시간 전역변수화 && GET method 에 상태코드 적용

--- app.ts
+++ app.ts
@@ -5,63 +5,75 @@ import fs from 'fs';
 const app = express();
 
 let limitTime: Date | undefined = undefined;
+const time_file_path = 'time.txt';
+const time_to_hell = 5;
 
-fs.readFile('time.txt', 'utf8', (err, data) => {
+fs.access('time.txt', fs.constants.F_OK | fs.constants.R_OK, (err) => {
     if (err) {
         console.error(err);
-        return;
+        limitTime = init_limit_time(time_file_path);
     } else {
-        limitTime = new Date(data);
-        console.log('Limit time:', limitTime);
+        console.log('Time File exists');
+        limitTime = read_time_file(time_file_path);
     }
-});
-
+})
 
 app.get('/time', (req: Request, res: Response) => {
   // return the remaining time
   // remaining time = limitTime - now
   if (limitTime === undefined) {
+    res.status(500);
     res.send('No limit time');
   } else {
     const now = new Date();
     const remainTime = limitTime.getTime() - now.getTime();
-    console.log(remainTime);
-    res.send(remainTime.toString());
+    // console.log(remainTime);
+    // res.send(remainTime.toString());
+    res.status(200);
+    res.send(get_time_in_form_DHMS(remainTime));
   }
 });
 
 app.post('/time', (req: Request, res: Response) => {
-  // reset limitTime to 1 week later
-    limitTime = refresh_limit_time();
-    res.send(limitTime.toString());
+    // set the limit time
+    // limitTime = now + 7 days
+    limitTime = init_limit_time(time_file_path);
+    res.send('Set limit time');
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-
-  if (limitTime === undefined) {
-    console.log('No limit time');
-    limitTime = new Date();
-    limitTime.setDate(limitTime.getDate() + 7);
-    console.log('Limit time refreshed :', limitTime);
-    fs.writeFile('time.txt', limitTime.toString() , (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('Limit time updated');
-    })
-  }
+    console.log('Example app listening on port 3000!');
+    console.log('Server Start with Limit time:', limitTime);
 });
 
-function refresh_limit_time() : Date {
+function init_limit_time(path : string) : Date {
     let new_limit_date : Date = new Date();
-    new_limit_date.setDate(new_limit_date.getDate() + 7);
-    fs.writeFile('time.txt', new_limit_date.toString() , (err) => {
-        if (err) {
-            console.error(err);
-            return undefined;
-        }
-    });
+    new_limit_date.setDate(new_limit_date.getDate() + time_to_hell);
+    fs.writeFileSync(path, new_limit_date.toString());
     return new_limit_date;
+}
+
+function read_time_file(path : string) : Date {
+    const Data : string = fs.readFileSync(path).toString();
+    const date : Date = new Date(Data);
+    if (date instanceof Date && !isNaN(date.getTime())) {
+        return date;
+    }
+    return undefined;
+}
+
+function get_time_in_form_DHMS(time_ms) {
+    const diffMs = time_ms; // 밀리초 단위 차이 계산
+    const diffSec = Math.floor(diffMs / 1000); // 초 단위 차이 계산
+    const diffMin = Math.floor(diffSec / 60); // 분 단위 차이 계산
+    const diffHr = Math.floor(diffMin / 60); // 시 단위 차이 계산
+    const diffDay = Math.floor(diffHr / 24); // 일 단위 차이 계산
+    const remainTime = {
+        day : diffDay,
+        hour : diffHr % 24,
+        minute : diffMin % 60,
+        second : diffSec % 60,
+        millisecond : diffMs % 1000
+    }
+    return remainTime;
 }
